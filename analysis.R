6dd5a804127fe7a2d0258b31f2d8248c770d8825
noahhhhhh <noah.xm.shaw@gmail.com>
2015-09-02 19:35:56 +1000

    a update

--- analysis.R
+++ analysis.R
@@ -1,5 +1,7 @@
 require(data.table)
 require(dplyr)
+require(ggplot2)
+require(scales)
 ############################################################
 ## 1. read the data ########################################
 ############################################################
@@ -11,10 +13,48 @@ load(filePath)
 ############################################################
 summary(dtProcessed)
 str(dtProcessed)
-## group by make stats #####################################
-dtByMake <- group_by(dtProcessed, make)
-statsMake <- summarise(dtByMake
-                       , meanPrice = mean(price, na.rm = T)
-                       , sdPrice = sd(price, na.rm = T)
-                       , cnt = n())
-View(arrange(statsMake, desc(meanPrice)))
+
+## 2.1 group by make stats #################################
+statsMake <- dtProcessed %>%
+    group_by(make) %>%
+    summarise(meanPrice = mean(price, na.rm = T)
+              , sdPrice = sd(price, na.rm = T)
+              , minPrice = min(price, na.rm = T)
+              , lowerPrice = quantile(price, na.rm = T)[2]
+              , midPrice = quantile(price, na.rm = T)[3]
+              , upperPrice = quantile(price, na.rm = T)[4]
+              , maxPrice = max(price, na.rm = T)
+              , cntMake = n()) %>%
+    arrange(desc(meanPrice), make)
+# plot price distribution by make
+# remove the most expensive car
+statsMakeAdj <- statsMake[meanPrice != max(statsMake$meanPrice, na.rm = T)]
+# sort the plot by meanPrice
+statsMakeAdj$make <- factor(statsMakeAdj$make, levels = statsMakeAdj$make[order(- statsMakeAdj$meanPrice, statsMakeAdj$make)])
+g_price_make <- ggplot(statsMakeAdj
+                       , aes(x = make
+                             , y = meanPrice
+                             , ymin = minPrice
+                             , lower = lowerPrice
+                             , middle = midPrice
+                             , upper = upperPrice
+                             , ymax = maxPrice))
+g_price_make <- g_price_make + geom_bar(stat = "identity", fill = "skyblue2")
+# g_price_make <- g_price_make + geom_boxplot(stat = "identity", fill = "skyblue2")
+g_price_make <- g_price_make + scale_y_continuous(name = "", labels = comma)
+g_price_make <- g_price_make + theme(plot.background = element_rect(fill = "white")
+               , axis.text.x = element_text(angle = 90, hjust = 1))
+g_price_make
+
+## 2.2 group bY make and year stats #########################
+statsMakeYear <- dtProcessed %>%
+    group_by(make, year) %>%
+    summarise(meanPrice = mean(price, na.rm = T)
+              , sdPrice = sd(price, na.rm = T)
+              , minPrice = min(price, na.rm = T)
+              , lowerPrice = quantile(price, na.rm = T)[2]
+              , midPrice = quantile(price, na.rm = T)[3]
+              , upperPrice = quantile(price, na.rm = T)[4]
+              , maxPrice = max(price, na.rm = T)
+              , cntMake = n()) %>%
+    arrange(desc(meanPrice), make)
